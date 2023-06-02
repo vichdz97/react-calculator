b882c9ff65164009e1bef4029e0017da4fff1cfc
Victor Hernandez <vichdz97@txstate.edu>
2023-06-02 16:50:33 -0500

    increased height of equal sign button

--- src/components/Operator.tsx
+++ src/components/Operator.tsx
@@ -1,11 +1,12 @@
 interface Props {
     children: string;
+    className?: string;
     onClick: () => void;
 }
 
-function Operator({ children, onClick }: Props) {
+function Operator({ children, className, onClick }: Props) {
     return (
-        <button className="btn btn-danger w-100 m-1" onClick={onClick}>{children}</button>
+        <button className={"btn btn-danger w-100 m-1 " + className} onClick={onClick}>{children}</button>
     )
 }
 
--- src/components/OperatorGrid.tsx
+++ src/components/OperatorGrid.tsx
@@ -1,3 +1,4 @@
+import "../App.css";
 import Operator from "./Operator";
 
 interface Props {
@@ -7,13 +8,13 @@ interface Props {
 
 function OperatorGrid({ logOperator, logResult }: Props) {
     return (
-        <div className="container w-25 operators">
+        <div className="container w-25">
             <div className="row row-cols-1">
                 <div className="col"><Operator onClick={() => logOperator("/")}>&divide;</Operator></div>
                 <div className="col"><Operator onClick={() => logOperator("*")}>&times;</Operator></div>
                 <div className="col"><Operator onClick={() => logOperator("-")}>-</Operator></div>
                 <div className="col"><Operator onClick={() => logOperator("+")}>+</Operator></div>
-                <div className="col"><Operator onClick={logResult}>=</Operator></div>
+                <div className="col"><Operator className="operator" onClick={logResult}>=</Operator></div>
             </div>
         </div>
     )
