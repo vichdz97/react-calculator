1cecca8e665a2a11063a2919db46412d32ba97f2
Victor Hernandez, Jr <vh4697@gmail.com>
2026-08-03 21:52:42 -0500

    replaced BrowserRouter with createHashRouter and RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { createHashRouter, Navigate, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
 import AppV2 from './AppV2.tsx'
 
+const router = createHashRouter([
+  { path: '/v2', element: <AppV2 /> },
+  { path: '/v1', element: <App /> },
+  { path: '*', element: <Navigate to='/v2' replace /> }
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/v2' element={<AppV2 />} />
-        <Route path='/v1' element={<App />} />
-        <Route path='*' element={<Navigate to='/v2' replace />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
